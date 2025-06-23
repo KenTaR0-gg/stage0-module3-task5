0b9ef3e339171aed8aad9a167ca5f3bee71210df
KenTaR0-gg <kotya.piso@gmail.com>
2025-06-23 03:53:16 +0300

    module 3. task 5.

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,8 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
-        int c = toBeDivided / 5;
+    void divide(double toBeDivided) {
+        double c = toBeDivided / 5;
         System.out.print(c);
     }
 }

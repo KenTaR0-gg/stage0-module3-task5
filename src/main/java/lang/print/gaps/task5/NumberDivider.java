58c8afa5a30661991b7cd1670becee8c3e77a32e
KenTaR0-gg <kotya.piso@gmail.com>
2025-06-23 14:21:45 +0300

    module 3. task 5.

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(double toBeDivided) {
         double c = toBeDivided / 5;
-        System.out.print(c);
+        System.out.println(c);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        float sec = 60*minutes;
-                System.out.print(sec);
+        float sec = 60 * minutes;
+        System.out.println(sec);
     }
 }
